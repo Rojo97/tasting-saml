bd1d2177ffb48247012839747b843b983d4565fb
Víctor Rojo <victor.rojoalvarez@telefonica.com>
2020-11-24 11:07:00 +0100

    initial commit

---
+++ identity_provider/src/main/java/com/rojo/tasting_saml2/ExampleIdentityProviderApplication.java
@@ -0,0 +1,13 @@
+package com.rojo.identity_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class ExampleIdentityProviderApplication {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleIdentityProviderApplication.class, args);
+    }
+ 
+}
---
+++ service_provider/src/main/java/com/rojo/service_provider/ExampleServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package com.rojo.service_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleServiceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleServiceProviderApplication.class, args);
+	}
+
+}
